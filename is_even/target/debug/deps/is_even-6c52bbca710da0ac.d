32cff97b078cc310dfa2749503738e3bfcd17225
Saikat123Dev <berasaikat729@gmail.com>
2024-09-11 23:47:42 +0530

    rust-first

---
+++ is_even/target/debug/deps/is_even-6c52bbca710da0ac.d
@@ -0,0 +1,5 @@
+/home/arch/my-workspace/dev-new/rust-learning/harkirat/is_even/target/debug/deps/is_even-6c52bbca710da0ac: src/main.rs
+
+/home/arch/my-workspace/dev-new/rust-learning/harkirat/is_even/target/debug/deps/is_even-6c52bbca710da0ac.d: src/main.rs
+
+src/main.rs:
---
+++ is_even/target/debug/is_even.d
@@ -0,0 +1,1 @@
+/home/arch/my-workspace/dev-new/rust-learning/harkirat/is_even/target/debug/is_even: /home/arch/my-workspace/dev-new/rust-learning/harkirat/is_even/src/main.rs
