cb08f041df290e78f89bbfcf2ff156ceab784141
Saikat123Dev <berasaikat729@gmail.com>
2024-09-12 00:23:50 +0530

    rust-2

---
+++ fib_ser/target/debug/fib_ser.d
@@ -0,0 +1,1 @@
+/home/arch/my-workspace/dev-new/rust-learning/harkirat/fib_ser/target/debug/fib_ser: /home/arch/my-workspace/dev-new/rust-learning/harkirat/fib_ser/src/main.rs
--- is_even/target/debug/is_even.d
+++
@@ -1,1 +0,0 @@
-/home/arch/my-workspace/dev-new/rust-learning/harkirat/is_even/target/debug/is_even: /home/arch/my-workspace/dev-new/rust-learning/harkirat/is_even/src/main.rs
