1ddc94c69ae35eaf45ec8e60f90234ee27e19ebe
Saikat123Dev <berasaikat729@gmail.com>
2024-09-12 00:26:29 +0530

    rust-2

--- fib_ser/target/debug/deps/fib_ser-46ef3f9e246bed65.d
+++
@@ -1,5 +0,0 @@
-/home/arch/my-workspace/dev-new/rust-learning/harkirat/fib_ser/target/debug/deps/fib_ser-46ef3f9e246bed65: src/main.rs
-
-/home/arch/my-workspace/dev-new/rust-learning/harkirat/fib_ser/target/debug/deps/fib_ser-46ef3f9e246bed65.d: src/main.rs
-
-src/main.rs:
--- fib_ser/target/debug/fib_ser.d
+++
@@ -1,1 +0,0 @@
-/home/arch/my-workspace/dev-new/rust-learning/harkirat/fib_ser/target/debug/fib_ser: /home/arch/my-workspace/dev-new/rust-learning/harkirat/fib_ser/src/main.rs
